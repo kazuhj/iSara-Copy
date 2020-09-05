01036f1d0239cdbbffd00c36ce75af072b4a9609
kazuhj <57065520+kazuhj@users.noreply.github.com>
2020-09-05 10:41:35 +0900

    initial commit

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+$(function() {
+  $('.panel').click(function() {
+    var $answer = $(this).find('.qa-answer');
+    if($answer.hasClass('open')) { 
+      $answer.removeClass('open');
+      // slideUpメソッドを用いて、$answerを隠してください
+      $answer.slideUp();
+
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('span').text('+');
+      
+    } else {
+      $answer.addClass('open'); 
+      // slideDownメソッドを用いて、$answerを表示してください
+      $answer.slideDown();
+      
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('span').text('-');
+      
+    }
+  });
+});
